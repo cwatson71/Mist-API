ce2af23a06a49280b265fa10623af58b2fac8a6a
Thomas Munzer <thomas.munzer@gmail.com>
2020-11-24 21:10:40 +0100

    fix get query bug

--- mlib/requests/orgs/ssoroles.py
+++ mlib/requests/orgs/ssoroles.py
@@ -2,8 +2,8 @@
 
 
 def get(mist_session, org_id, page=1, limit=100):
-    uri = "/api/v1/orgs/%s/ssoroles" % org_id
-    resp = mist_session.mist_get(uri, org_id, page=page, limit=limit)
+    uri = "/api/v1/orgs/%s/ssoroles" % org_id    
+    resp = mist_session.mist_get(uri, page=page, limit=limit)
     return resp
 
 def create(mist_session, org_id, ssorole):
